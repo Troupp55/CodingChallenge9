c8fa34fbf1ae1e78e30541be6cb2a99f6467e7c1
Tristen Roupp <tristenr@usf.edu>
2024-06-14 13:21:19 -0400

    Implement interactive behaviors with D3.js

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const data = [10, 20, 30, 40, 50]; // Example data
+
+    d3.select("body")
+        .selectAll(".data-point")
+        .data(data)
+        .enter()
+        .append("div")
+        .attr("class", "data-point")
+        .on("mouseover", function() {
+            d3.select(this).style("background-color", "firebrick");
+        })
+        .on("mouseout", function() {
+            d3.select(this).style("background-color", "lightblue");
+        });
+});
